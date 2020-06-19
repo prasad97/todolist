ad4f79f05f1d69a0433e3c3fbb20d0cedd07cf8d
Prasad Sawant <prasad.sawant20397@gmail.com>
2020-06-20 01:42:52 +0530

    added ejs

--- app.js
+++ app.js
@@ -3,15 +3,23 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
+app.set('view engine','ejs');
+
 app.get('/', function(req,res){
     // res.send("Hello");
     var today = new Date();
+    var currentDay = today.getDay();
+    var day = '';
 
-    if(today.getDay()===6 || today.getDay()===0){
-        res.send('Yay its a weekend!');
+    if(currentDay===6 || currentDay===0){
+        // res.send('<h1>Yay its a weekend!</h1>');
+        day = 'Weekend';
     } else{
-        res.send('Boo I have to work today');
+        // res.send('Boo I have to work today');
+        // res.sendFile(__dirname + '/index.html');
+        day = 'Weekday';
     }
+    res.render('list',{kindOfDay:day});
 });
 
 app.listen(3000, function(){
